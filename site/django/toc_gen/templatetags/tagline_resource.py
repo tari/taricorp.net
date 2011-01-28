b6927db168fd40c1bb7addf5ea68a8b4740a28aa
Tari <devnull@localhost>
2011-01-28 14:54:50 -0500

    Added tagline generator.

---
+++ site/django/toc_gen/templatetags/tagline_resource.py
@@ -0,0 +1,8 @@
+taglines = [
+    'More fun than a kick in the teeth',
+    'Not a sausage',
+    "Don't panic",
+    'Where good ideas come to die',
+    'Better than an iron sunrise',
+    'Burning paradise to the ground',
+]
