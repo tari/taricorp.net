016d122711d01ec2a4f555e891c59877944619c3
Peter Marheine <peter@taricorp.net>
2017-02-26 20:32:51 +1100

    Delete the old site.
    Replacing it again.

--- mkblog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Entry title: " TITLE
-SLUG=$(echo $TITLE | tr " " "-")
-read -p "Slug [$SLUG]: " SLUG
-
-NOW=`date +%Y-%m-%d`
-sed s/CREATION_DATE/`date +%Y-%m-%d`/g blog-template.html > site/content/blog/`date +%Y`/$SLUG.html
-
