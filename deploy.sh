c260979bf6fe78d2d2d9f683a97525eafc091177
Peter Marheine <peter@taricorp.net>
2017-05-22 19:57:46 +1000

    Add options to deploy script, fix permissions on PDFs.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
-hugo && rsync -azx --delete public/ shirabe:/var/www/beta.taricorp.net/public_html/
+typeset build=true
+
+while [ "$#" -gt "0" ]
+do
+    case $1 in
+        --no-build)
+            build=false
+            ;;
+        --debug)
+            set -x
+            ;;
+        *)
+            echo "Unrecognized option: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$build" = "true" ]
+then
+    hugo
+fi
+
+rsync -azx --delete public/ shirabe:/var/www/beta.taricorp.net/public_html/
